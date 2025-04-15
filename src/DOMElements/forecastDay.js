d2e2bc60460bc2dde82ecd12906ee7f6f79aca05
Jan <jas.gol@poczta.onet.pl>
2025-04-15 18:42:55 +0200

    add icons to forecastDays

--- src/DOMElements/forecastDay.js
+++ src/DOMElements/forecastDay.js
@@ -1,17 +1,19 @@
-export default function forecastDay(dayInfo){
-    const container = document.createElement('div');
-    for(let info in dayInfo) {
-        if(info==="icon"){
-            import(`../icons/${dayInfo[info]}.svg`).then((module)=>{
-                console.log(module)
-            })
-        }
-        else{
-            const infoParagraph = document.createElement("p");
-            infoParagraph.textContent=dayInfo[info];
-            container.appendChild(infoParagraph);
-        }
+export default function forecastDay(dayInfo) {
+  const container = document.createElement("div");
+  for (let info in dayInfo) {
+    if (info === "icon") {
+      const img = document.createElement("img");
+        img.setAttribute("class", "weather-icon");
+        import(`../icons/${dayInfo[info]}.svg`).then((module) => {
+        img.setAttribute("src", module.default);
+    });
+    container.appendChild(img);
+    } else {
+      const infoParagraph = document.createElement("p");
+      infoParagraph.textContent = dayInfo[info];
+      container.appendChild(infoParagraph);
     }
+  }
 
-    return container;
+  return container;
 }
